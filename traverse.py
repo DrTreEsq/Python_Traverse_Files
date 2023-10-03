3fa9460f7f1bef6831ccc2f0187526b6ce0b1bb2
The Ethical Mad Scientist <117105883+TREYCSE@users.noreply.github.com>
2023-10-03 10:07:01 -0400

    Update traverse.py

--- traverse.py
+++ traverse.py
@@ -1,1 +1,3 @@
-
+# Make your Python script accept an argument to traverse any path (don't hardcode the path!)
+# Use different options in the script for reporting, like:
+# No more than 20 files, & Files larger than a specific size in megabytes
